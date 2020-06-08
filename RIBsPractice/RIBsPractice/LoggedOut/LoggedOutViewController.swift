62552178246fa9ab73bd89ba3159ab3c27d39fb2
unnnyong <unnnyong@gmail.com>
2020-06-08 16:11:55 +0900

    setup RIBs, add LoggedOut RIB

---
+++ RIBsPractice/RIBsPractice/LoggedOut/LoggedOutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoggedOutViewController.swift
+//  RIBsPractice
+//
+//  Created by Eunyeong Kim on 2020/06/08.
+//  Copyright © 2020 Eunyeong Kim. All rights reserved.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+
+protocol LoggedOutPresentableListener: class {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class LoggedOutViewController: UIViewController, LoggedOutPresentable, LoggedOutViewControllable {
+
+    weak var listener: LoggedOutPresentableListener?
+}
