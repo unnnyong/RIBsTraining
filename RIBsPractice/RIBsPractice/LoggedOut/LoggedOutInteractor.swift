862de1301b40da963205dde104927b37ca5ce840
unnnyong <unnnyong@gmail.com>
2020-06-10 16:21:17 +0900

    사용하지않는 override method와 TODO 코멘트 삭제

--- RIBsPractice/RIBsPractice/LoggedOut/LoggedOutInteractor.swift
+++ RIBsPractice/RIBsPractice/LoggedOut/LoggedOutInteractor.swift
@@ -9,13 +9,10 @@
 import RIBs
 import RxSwift
 
-protocol LoggedOutRouting: ViewableRouting {
-    // TODO: Declare methods the interactor can invoke to manage sub-tree via the router.
-}
+protocol LoggedOutRouting: ViewableRouting {}
 
 protocol LoggedOutPresentable: Presentable {
     var listener: LoggedOutPresentableListener? { get set }
-    // TODO: Declare methods the interactor can invoke the presenter to present data.
 }
 
 protocol LoggedOutListener: class {
@@ -27,21 +24,10 @@ final class LoggedOutInteractor: PresentableInteractor<LoggedOutPresentable>, Lo
     weak var router: LoggedOutRouting?
     weak var listener: LoggedOutListener?
 
-    // TODO: Add additional dependencies to constructor. Do not perform any logic
-    // in constructor.
     override init(presenter: LoggedOutPresentable) {
         super.init(presenter: presenter)
+
         presenter.listener = self
-    }
-
-    override func didBecomeActive() {
-        super.didBecomeActive()
-        // TODO: Implement business logic here.
-    }
-
-    override func willResignActive() {
-        super.willResignActive()
-        // TODO: Pause any business logic.
     }
 }
 
