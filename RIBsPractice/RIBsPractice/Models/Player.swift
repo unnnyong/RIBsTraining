a4e6271ae4bdd9b15d8c9ed2b85cf9bed3393f52
unnnyong <unnnyong@gmail.com>
2020-06-14 20:12:10 +0900

    Player model 추가

---
+++ RIBsPractice/RIBsPractice/Models/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  RIBsPractice
+//
+//  Created by Eunyeong Kim on 2020/06/14.
+//  Copyright © 2020 Eunyeong Kim. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    let name: String
+    let type: PlayerType
+}
